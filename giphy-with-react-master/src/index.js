9e23d0648794a4f7ac599130ceb310f9a157dc9b
Your Name <you@example.com>
2019-06-02 13:39:14 +0400

    giphy

---
+++ giphy-with-react-master/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+import axios from "axios"
+
+let giphy = []
+
+axios.get("https://api.giphy.com/v1/gifs/search?api_key=O5LyiGH753b9EWs8TEN6LL0DLtx8wqNm&q=cat&limit=25&offset=0&rating=G&lang=en").then((response)=>{
+     giphy = response.data.data.map((value,index)=>{
+         return (
+             <li key={index}><img src ={value.images["480w_still"].url} alt="cat"/></li>
+         )
+     })
+     ReactDOM.render(<ul>{giphy}</ul>,document.querySelector("#root"));
+})
+
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
